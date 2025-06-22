9a46d6bcdd681069491dac311ce3fea47ad98ea9
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-06-22 21:02:55 +0100

    generate drop down menus and images for each queen

--- script.js
+++ script.js
@@ -12,27 +12,42 @@ const competitionData = {
                "The queens design and market drag-influenced products. Entrepreneur Marcus Lemonis (CNBC’s The Profit) co-stars, and comedian Graham Norton guest judges.",
                "The queens must makeover family members and vogue the house down. Aubrey Plaza (Parks and Recreation) guest judges.",
                "The winner of RuPaul’s Drag Race All Stars is crowned. But first, the queens must write original rap lyrics and perform as the ultimate girl group."] ,
-    runways: [null, "Latex Eleganza", "The Future of Drag", "Two Looks in One", null, "Pants on the Runway", "Makeover Challenge", "Final Eleganza"]
+    runways: [null, "Latex Eleganza", "The Future of Drag", "Two Looks in One", null, "Pants on the Runway", "Makeover Challenge", "Final Eleganza"],
+    placements: ["Win", "Top 2", "High", "Safe", "Low", "Bottom", "Eliminated", "Out", "Quit"]
+}
+
+const competitionMechanics = {
+    calculatePoints: function(placement) {
+        if (placement="Win") {let points=6}
+        else if (placement==="Top 2") {points=5}
+        else if (placement==="High") {points=4}
+        else if (placement==="Safe") {points=3}
+        else if (placement==="Low") {points=2}
+        else if (placement==="Bottom") {points=1}
+        else if (placement==="Eliminated") {points=0}
+        else if (placement==="Out" || placement==="Quit") {points=0};
+        return points;
+    }
 }
 
 const queens = (function () {
-    numberOfQueens = 10;
-
     queen0 = {
         queen: "Adore Delano",
         img: "images/AdoreDelanoAS2.webp",
-        progress: ["Bottom", "Quit", "Out", "Out", "Out", "Out", "Out", "Out"],
-        return: [false, false, false, false, false, false, false, false]
+        placement: ["Bottom", "Quit", "Out", "Out", "Out", "Out", "Out", "Out"],
+        return: Array(competitionData.numberOfWeeks, false)
     };
 
     queen1 = {
         queen: "Alaska",
         img: "images/AlaskaAS2.webp",
-        progress: ["High", "Win", "Safe", "Win", "Win", "Win", "Bottom", "Win"],
-        return: [false, false, false, false, false, false, false, false]
+        placement: ["High", "Win", "Safe", "Win", "Win", "Win", "Bottom", "Win"],
+        return: Array(competitionData.numberOfWeeks, false)
     };
 
-    return {numberOfQueens, queen0, queen1};
+    const queens = [queen0, queen1];
+    const numberOfQueens = queens.length;
+    return {queens, numberOfQueens};
 })();
 
 const images = {
@@ -47,7 +62,7 @@ const divs = {
 }
 
 const display = (function () {
-    const createDropdown = function() {
+    const createWeeksDropdown = function() {
         // Create the select dropdown
         const select = document.createElement("select");
         select.id = "week-select";
@@ -81,13 +96,61 @@ const display = (function () {
         rightArrow.src = images.arrowRight;
         rightArrow.width = 60;
 
-        dropdown = createDropdown ();
+        const dropdown = createWeeksDropdown ();
 
         divs.nav.appendChild(leftArrow);
         divs.nav.appendChild(dropdown.select);
         divs.nav.appendChild(rightArrow);
+    }
 
-        update();
+    const createPlacementDropdown = function() {
+        // Create the select dropdown
+        const select = document.createElement("select");
+        select.className = "placement-select";
+
+        // Add options from 1 to numberOfWeeks
+        for (let i = 0; i <= competitionData.placements.length-1; i++) {
+            const option = document.createElement("option");
+            // option.value = ;
+            option.textContent = competitionData.placements[i];
+            option.value = competitionData.placements[i];
+            select.appendChild(option);
+        }
+
+        // Append label and select to the container
+        return {select}
+    }
+    
+    const updatePlacementDropdowns = function() {
+        for (let i = 0; i < queens.numberOfQueens; i++) {
+            const queenDropdown = document.getElementById(`queen-dropdown${i}`);
+            const queenImage = document.getElementById(`queen-image${i}`);
+            const placementAtWeek = queens.queens[i].placement[week-1];
+
+            queenDropdown.value = placementAtWeek;
+            queenDropdown.style.display = placementAtWeek!=="Out" ? "inline-block" : "none";
+
+            queenImage.className = "queen-image " + placementAtWeek.toLowerCase().replaceAll(" ", "");
+
+            // if (placementAtWeek==="Out") {queenImage.className="queen-image out"}
+            // else {queenImage.className="queen-image"};
+        }
+    }
+
+    const displayQueens = function() {
+        const displayObjects = new Array(queens.numberOfQueens);
+        for (let i = 0; i < queens.numberOfQueens; i++) {
+            displayObjects[i]=document.createElement("div");
+            displayObjects[i].id=`queen${i}`;
+            displayObjects[i].innerHTML=`<img src=${queens.queens[i].img} class="queen-image" id="queen-image${i}">
+                                         <h3 class="queen-name">${queens.queens[i].queen}</h3>`;
+
+            const dropdown = createPlacementDropdown();
+            dropdown.select.id=`queen-dropdown${i}`;
+            divs.queens.appendChild(displayObjects[i]);
+            const innerDiv = document.getElementById(`queen${i}`);
+            innerDiv.append(dropdown.select);
+        }
     }
 
     const challengeHeaders = function() {
@@ -107,14 +170,17 @@ const display = (function () {
         document.getElementById("left-arrow").style.display = week > 1 ? "inline-block" : "none";
         document.getElementById("right-arrow").style.display = week < competitionData.numberOfWeeks ? "inline-block" : "none";
 
-        const dropdown = document.getElementById("week-select");
-        dropdown.value = week.toString();
+        const weekDropdown = document.getElementById("week-select");
+        weekDropdown.value = week.toString();
 
         challengeHeaders();
+
+        updatePlacementDropdowns();
     }
 
     const init = function() {
         displayArrows();
+        displayQueens();
     }
 
     return {init, update};
@@ -145,4 +211,5 @@ const interface = (function () {
 })();
 
 display.init();
+display.update();
 interface.arrowListeners();
