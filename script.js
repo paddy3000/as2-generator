e0db65555c380ebc8cc86a3d938ea040ba6b28dc
paddy3000 <paddyohara1998@hotmail.co.uk>
2025-06-22 12:32:00 +0100

    add titles, lip-sync songs and runway text

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var week=1;
 
 const competitionData = {
-    numberOfWeeks: 8
+    numberOfWeeks: 8,
+    episodes: ["All Star Talent Show Extravaganza", "All Stars Snatch Game", "HERstory of the World", "Drag Movie Shequels", "Revenge Of The Queens", "Drag Fish Tank", "Family That Drags Together", "All Stars Supergroup"],
+    lipSyncs: ["Shake It Off by Taylor Swift", "Le Freak (Freak Out) by Chic", "Tell It To My Heart by Taylor Dayne", "Got To Be Real by Cheryl Lynn", "Shut Up & Drive by Rihanna", "Cherry Bomb by Joan Jett and the Blackhearts", "Step It Up by RuPaul feat. Dave Audé", "If I Were Your Woman by Gladys Knight & The Pips"],
+    synopses: ["10 queens return to compete for $100,000 and a place in the Drag Race Hall of Fame. The first test is to compete in a drag talent show. With guest judge Raven-Symoné.",
+               "The queens impersonate celebrities in a quick-witted TV game show and burn rubber in a latex runway. With guest judge Ross Mathews.",
+               "The queens are challenged to perform in a lip-syncing dance number inspired by the legendary historical women. Fashion designer Jeremy Scott guest judges.",
+               "The queens' acting chops are tested in parody movie sequels of RuPaul's favorite movies. Pussycat Doll Nichole Scherzinger guest judges.",
+               "The queens must perform a live stand-up comedy act in front of an audience of judgmental drag queens. Ross Mathews returns to guest judge.",
+               "The queens design and market drag-influenced products. Entrepreneur Marcus Lemonis (CNBC’s The Profit) co-stars, and comedian Graham Norton guest judges.",
+               "The queens must makeover family members and vogue the house down. Aubrey Plaza (Parks and Recreation) guest judges.",
+               "The winner of RuPaul’s Drag Race All Stars is crowned. But first, the queens must write original rap lyrics and perform as the ultimate girl group."] ,
+    runways: [null, "Latex Eleganza", "The Future of Drag", "Two Looks in One", null, "Pants on the Runway", "Makeover Challenge", "Final Eleganza"]
 }
 
 const images = {
@@ -10,8 +21,9 @@ const images = {
 }
 
 const divs = {
-    queensDiv: document.getElementById("queens-div"),
-    navDiv: document.getElementById("nav-div")
+    queens: document.getElementById("queens-div"),
+    nav: document.getElementById("nav-div"),
+    episodeInfo: document.getElementById("episode-info")
 }
 
 const display = (function () {
@@ -33,7 +45,7 @@ const display = (function () {
     }
 
     const displayArrows = function() {
-        divs.navDiv.innerHTML="";
+        divs.nav.innerHTML="";
 
         const leftArrow = document.createElement("input");
         leftArrow.type = "image";
@@ -51,11 +63,24 @@ const display = (function () {
 
         dropdown = createDropdown ();
 
-        divs.navDiv.appendChild(leftArrow);
-        divs.navDiv.appendChild(dropdown.select);
-        divs.navDiv.appendChild(rightArrow);
+        divs.nav.appendChild(leftArrow);
+        divs.nav.appendChild(dropdown.select);
+        divs.nav.appendChild(rightArrow);
 
         update();
+    }
+
+    const challengeHeaders = function() {
+        title  = competitionData.episodes[week-1];
+        lipSync = competitionData.lipSyncs[week-1];
+        synopsis = competitionData.synopses[week-1];
+        runway = competitionData.runways[week-1];
+
+        divs.episodeInfo.innerHTML = `
+            <h2 id="episode-title">${title}</h2>
+            <p id="synopsis">${synopsis}</p>
+            ${runway ? `<p id="runway-theme"><b>Runway theme:</b> ${runway}</p>` : ""}
+            <p id="lip-sync-song"><b>Lip-sync song:</b> ${lipSync}</p>`;
     }
 
     const update = function() {
@@ -64,6 +89,8 @@ const display = (function () {
 
         const dropdown = document.getElementById("week-select");
         dropdown.value = week.toString();
+
+        challengeHeaders();
     }
 
     const init = function() {
@@ -76,7 +103,7 @@ const display = (function () {
 
 const interface = (function () {
     const arrowListeners = function () {
-        divs.navDiv.addEventListener("click", function (e) {
+        divs.nav.addEventListener("click", function (e) {
             if (e.target.id === "left-arrow" && week > 1) {
                 week--;
                 display.update();
@@ -86,7 +113,7 @@ const interface = (function () {
             }
         });
 
-        divs.navDiv.addEventListener("change", function (e) {
+        divs.nav.addEventListener("change", function (e) {
             if (e.target.id === "week-select") {
                 week = parseInt(e.target.value);
                 display.update();
